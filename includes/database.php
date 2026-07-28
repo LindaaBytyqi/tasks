86b1d7b37b7d8f74980d840a09d417683fb241a0
LindaaBytyqi <149429325+LindaaBytyqi@users.noreply.github.com>
2026-07-28 16:14:54 +0200

    initalize project and database connection

---
+++ includes/database.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$port = "5432";
+$dbname = "tasksproject";
+$username = "postgres";
+$password = "karrota";
+
+try {
+    $conn = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $username,
+        $password
+    );
+    $conn->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+} catch(PDOException $e){
+    die("Connection failed: " . $e->getMessage());
+}
+
+?>
